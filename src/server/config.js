6653f683d2be853586687d68ac0f4ffddf361ac2
Edgar Alejandro <edgarjardines7@gmail.com>
2019-01-10 12:08:55 -0600

    Creación del servidor

---
+++ src/server/config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const exphbs = require('express-handlebars');
+const morgan = require('morgan');
+const multer = require('multer');
+const express = require('express');
+const routes = require('../routes/index')
+const errorHandler = require('errorhandler')
+
+module.exports = app => {
+  //Settings
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('.hbs', exphbs({
+    defaultLayout: 'main',
+    partialsDir: path.join(app.get('views'), 'partials'),
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    extname: '.hbs',
+    helpers: require('./helpers')
+  }));
+  app.set('view engine', '.hbs');
+
+  // middelwares
+  app.use(morgan('dev'));
+  app.use(multer({
+    desr: path.join(__dirname, '../public/upload/temp')
+  }).single('image'));
+  app.use(express.urlencoded({
+    extendend: false
+  }));
+  app.use(express.json());
+
+
+  // routes
+  routes(app);
+
+  //static files
+  app.use('/public', express.static(path.join(__dirname, '../public')));
+
+  // errorhandlers
+  if ('development' === app.get('env')) {
+    app.use(errorHandler);
+  }
+
+  return app;
+}
