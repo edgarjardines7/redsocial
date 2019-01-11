b40a06c10413bfcf7d25128dcb00e0924fd6e141
Edgar Alejandro <edgarjardines7@gmail.com>
2019-01-10 18:58:14 -0600

    Motor de plantillas y vistas

--- src/server/config.js
+++ src/server/config.js
@@ -9,8 +9,8 @@ const errorHandler = require('errorhandler')
 module.exports = app => {
   //Settings
   app.set('port', process.env.PORT || 3000);
-  app.set('views', path.join(__dirname, 'views'));
-  app.set('.hbs', exphbs({
+  app.set('views', path.join(__dirname, '../views'));
+  app.engine('.hbs', exphbs({
     defaultLayout: 'main',
     partialsDir: path.join(app.get('views'), 'partials'),
     layoutsDir: path.join(app.get('views'), 'layouts'),
